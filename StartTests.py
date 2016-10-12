2d90a376127757bbb87aac5889c016eba106ada9
anx00209 <risogolo@gmail.com>
2016-10-12 16:18:03 +0200

    pokus2

--- StartTests.py
+++ StartTests.py
@@ -1,16 +1,31 @@
+
 import unittest
 from start import ProducerThread
+from start import ConsumerThread
 
 if __name__ == '__main__':
     unittest.main()
 
-class WidgetTestCase(unittest.TestCase):
+#  dont know how create a test suite and i dont know why the main application is executed with tests
+class MyTest(unittest.TestCase):
     def setUp(self):
-        self._pokus = ProducerThread.process2('The widget')
+        self.pt = ProducerThread()
+        self.ct = ConsumerThread()
 
+    def MarkupLengthHigherThanZero(self):
+        markup = self.pt.process('http://www.eset.sk')
+        self.assertGreater(len(markup.decode(encoding='UTF-8')), 0, "not markup returned")
+
+    def Test(self):
+        markup = self.pt.process('http://www.eset.sk')
+        links = self.ct.processlinks(markup)
+        self.assertEqual(links[0], 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd', "no links found")
+'''
     def tearDown(self):
-        self._pokus.dispose()
-        self._pokus = None
+        self.pt.dispose()
+        self.pt = None
 
-    def test_process(self):
-        self.assertEqual(self, self._pokus, 'The widget', 'aaa')
+        self.ct.dispose()
+        self.pt = None
+'''
+
--- start.py
+++ start.py
@@ -2,71 +2,72 @@ import time
 import urllib.request
 from multiprocessing import Queue
 from threading import Thread
-#from urllib.parse import urljoin
 import urllib
-#import lxml.html
 import re
 
-pages = ['http://www.pc.sk', 'http://www.sme.sk'] #zbytocne lebo idem so suboru
-q = Queue(0)
-pool = multiprocessing.Pool(processes=4)
+q = Queue(0) #  it looks like concurent object, no locking is necessary, but I might be wrong, instantiated with 0 wich means infinity
 
 
 class ProducerThread(Thread):
+    # it is automaticaly called when method start is called on instance of this class
     def run(self):
-        file = open('newfile.txt', 'r')
-        uris = file.readlines()
-        for url in uris:
-            try:
-                req = urllib.request.Request(url)
-                with urllib.request.urlopen(req) as response:
-                    the_page = response.read()
-                    global q
-                q.put(the_page)
-                print('produced')
-                process(req.full_url)
-            except Exception as ex:
-                pass
+        uris = []
+        try:
+            file = open('newfile.txt', 'r')
+            uris = file.readlines()
+        except Exception as ex:  #  I dont know how to determine what kind of exception may happen, 1st link in file is intentionally wrong
+            print("something went wrong with the file")
+            #  dont know how to terminate the process/thread here
+            pass
 
-            #pool.map(process_url, list_of_urls)
+        self.in_parallel(self.enqueue, uris)  # it will start as many processes as many the links in the list, which is not ok by my opinion, there should be predefined amount of possible threads
 
-def process():
-    print(str)
+    # I don't know ho to make this method kind of private for better encapsulation
+    def in_parallel(self, fn, l):
+        for i in l:
+            Thread(target=fn, args=(i,)).start()
 
-def process2(str):
-    return str+1
+    def enqueue(self, url):
+        try:
+            link = self.process(url)
+            global q  # don't know what this is for?
+            q.put(link)
+            print('produced')
+        except Exception as ex:  # don't know how to work with exceptions properly in python
+            print('invalid link or something else happened')
+            pass
+
+    def process(self, url):
+        req = urllib.request.Request(url)  # it looks like when the link would not be parsed because of the format of an hypelink, then error is thrown
+        with urllib.request.urlopen(req) as response:
+
+            '''if response.status_code == 200:
+                print('OK')'''  # does not work, don't know why, I would treat issues based on status codes otherwise
+            the_page = response.read() #  it returns object, I don't know how to select only markup from that object in python
+            return the_page
+
 
 class ConsumerThread(Thread):
+    # it is automaticaly called when method start is called on instance of this class
     def run(self):
-        dequeueAndProcess()
+        self.dequeue()
 
-def dequeueAndProcess():
-    global q
-    while True:
-        time.sleep(2) #just to prove is asynchronous
-        the_page = q.get()
-        a = the_page.decode(encoding='UTF-8') # don't know how to extract only markup from that object
-        # q.task_done() only in diferent version of the library
-        links = re.findall('"((http|ftp)s?://.*?)"', a)
-        print(links)
-        print('consumed')
+    def dequeue(self):
+        global q
+        while True:
+            time.sleep(2)  # just to prove is asynchronous, you can remove it
+            the_page = q.get()
+            links = self.processlinks(the_page)
+            print(links)
+            print('consumed')
+
+    def processlinks(self, page):
+        a = page.decode(encoding='UTF-8')  # don't know how to extract only markup from that object
+        # q.task_done() only exist in different version of the library
+        links = re.findall('"((http|ftp)s?://.*?)"', a)  # it searches for hyperlinks
+        return links
+
 
 
 ProducerThread().start()
 ConsumerThread().start()
-
-class WidgetTestCase(unittest.TestCase):
-    def setUp(self):
-        self.widget = ProducerThread()
-        self.widget.pr
-
-    def tearDown(self):
-        self.widget.dispose()
-        self.widget = None
-
-    def test_default_size(self):
-        self.assertEqual(self.widget.size(), (50,50), 'incorrect default size')
-
-    def test_resize(self):
-        self.widget.resize(100,150)
-        self.assertEqual(self.widget.size(), (100,150), 'wrong size after resize')
